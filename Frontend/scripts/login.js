1f30bb2591b8909a1d9d5a2b9cc6d6137bde04af
Ashaypatakare04 <patakareashay@gmail.com>
2026-01-26 18:48:31 +0530

    Separation of HTML & JS

---
+++ Frontend/scripts/login.js
@@ -0,0 +1,55 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBEyRxlEO4JKzeRBqLTBqUQS-abtp58eaw",
+  authDomain: "pulsevoice-1ef60.firebaseapp.com"
+});
+
+const auth = firebase.auth();
+
+// Toggle
+showSignup.onclick = () => {
+  signupSection.classList.remove("hidden");
+  loginSection.classList.add("hidden");
+  showSignup.classList.add("bg-white/20");
+  showLogin.classList.remove("bg-white/20");
+};
+
+showLogin.onclick = () => {
+  loginSection.classList.remove("hidden");
+  signupSection.classList.add("hidden");
+  showLogin.classList.add("bg-white/20");
+  showSignup.classList.remove("bg-white/20");
+};
+
+// Login
+loginForm.onsubmit = e => {
+  e.preventDefault();
+  auth.signInWithEmailAndPassword(loginEmail.value, loginPassword.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => alert(err.message));
+};
+
+// Signup
+signupForm.onsubmit = e => {
+  e.preventDefault();
+  if (signupPassword.value !== signupConfirm.value)
+    return alert("Passwords do not match");
+  auth.createUserWithEmailAndPassword(signupEmail.value, signupPassword.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => alert(err.message));
+};
+
+// Google
+googleSignIn.onclick = () => {
+  auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then(() => location.href = "dashboard.html")
+    .catch(err => alert(err.message));
+};
+
+// Reset
+forgotPassword.onclick = e => {
+  e.preventDefault();
+  if (!loginEmail.value) return alert("Enter email");
+  auth.sendPasswordResetEmail(loginEmail.value)
+    .then(() => alert("Reset link sent"))
+    .catch(err => alert(err.message));
+};
